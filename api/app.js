b0d02a2637f793492485576174a816a101329f3d
yash-1025 <yashpathak_22474@aitpune.edu.in>
2024-07-30 00:10:25 +0530

    yash12

--- api/app.js
+++ api/app.js
@@ -11,7 +11,7 @@ import messageRoute from "./routes/message.route.js";
 const app = express();
 
 
-app use
+
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
